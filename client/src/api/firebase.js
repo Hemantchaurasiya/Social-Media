070aa617e1755bdf22566a250872bbc6ba5cd07a
Hemant Chaurasiya <77195882+Hemantchaurasiya@users.noreply.github.com>
2023-01-24 12:46:30 -0800

    Update firebase.js
    update firebase api url

--- client/src/api/firebase.js
+++ client/src/api/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getStorage} from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAScLwsQV_aeT63moZ7op02iXI9kzVuQo0",
-  authDomain: "socialmedia-59503.firebaseapp.com",
-  projectId: "socialmedia-59503",
-  storageBucket: "socialmedia-59503.appspot.com",
-  messagingSenderId: "836172652549",
-  appId: "1:836172652549:web:ec8c83eee68c3bc4b7fc6c",
-  measurementId: "G-E80X56CDW8"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
